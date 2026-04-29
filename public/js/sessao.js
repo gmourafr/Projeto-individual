551fd8a07663836526f4a2eacb64beaba1a70075
Guilherme Ferreira Moura <guilherme.fmoura@sptech.school>
2026-04-28 21:31:18 -0300

    Algumas correções e junção dos scripts
    Juntando scrips em um .js somente

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -34,3 +34,70 @@ function finalizarAguardar(texto) {
     }
 }
 
+
+
+// JS DA CALCULADORA DE KD 
+let limites = [0.5, 0.8, 1.0, 1.2, 1.5, 2.5]
+let frases = [
+    "vai treinar moleque, tá mais perdido que flash no pé",
+    "ainda tô te olhando... mas esse KD tá feio hein",
+    "tá no caminho, mas ainda tá mais perdido que cego em tiroteio",
+    "ok, agora você existe no jogo",
+    "agora sim, digno de não ser kickado",
+    "calma aí… tá carregando ou só caiu contra noob?",
+    "Pode BANIR ESSE CARA VALVE",
+
+]
+
+function calcular() {
+    let kills = Number(ipt_kills.value)
+    let mortes = Number(ipt_mortes.value)
+
+    if (kills <= 0 || mortes <= 0) {
+        div_resultado.innerHTML = `insira valores válidos!`
+    } else {
+        let kd = kills / mortes
+        let frase = frases[frases.length - 1]
+        let achou = false
+
+        for (let i = 0; i < limites.length; i++) {
+            if (kd < limites[i] && achou == false) {
+                frase = frases[i]
+                achou = true
+            }
+        }
+        div_resultado.innerHTML = `
+            seu K/D é de: <b>${kd.toFixed(2)}<b> <br>
+            Professor FalleN diz: ${frase}
+        `
+    }
+}
+
+
+
+function entrar() {
+    let email = email_input.value
+    let senha = senha_input.value
+
+    if (email == '' || senha == '') {
+        div_msg.innerHTML = `preencha todos os campos!`
+    } else {
+        let nome = sessionStorage.getItem('NOME_USUARIO')
+        let emailSalvo = sessionStorage.getItem('EMAIL_USUARIO')
+
+        if (email == emailSalvo) {
+            div_msg.innerHTML = `bem-vindo de volta, ${nome}! redirecionando...`
+            setTimeout(() => {
+                window.location = 'dashboard/dashboard.html'
+            }, 2000)
+        } else {
+            div_msg.innerHTML = `e-mail ou senha incorretos!`
+        }
+    }
+}
+
+
+
+
+
+
